1502de49c32f1218bc6f0c5fd5bc79f06a7223c9
Kha <nlhkha@gmail.com>
2022-02-06 14:42:08 +0000

    basic article insert

--- src/lib.rs
+++ src/lib.rs
@@ -1,77 +1,102 @@
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::collections::{UnorderedMap};
+use near_sdk::collections::{UnorderedMap, Vector};
 use near_sdk::{env, near_bindgen, AccountId};
+use serde::{Serialize, Deserialize};
 
 near_sdk::setup_alloc!();
 
+#[derive(Serialize)]
+pub struct Article {
+    id: u64,
+    title: String,
+    content: String,
+    author: AccountId,
+}
 
+#[derive(Serialize, Deserialize, BorshSerialize, BorshDeserialize)]
+pub struct ArticleMeta {
+    title: String,
+    author: AccountId,
+}
 
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize)]
 pub struct Wiki {
-    len: u64,
-    titles: UnorderedMap<u64, String>,
+    meta: UnorderedMap<u64, ArticleMeta>,
     corpus: UnorderedMap<u64, String>,
-    authors: UnorderedMap<u64, AccountId>,
 }
 
 impl Default for Wiki {
     fn default() -> Self {
         Self {
-            len: 0,
-            titles: UnorderedMap::new(b"titles".to_vec()),
+            meta: UnorderedMap::new(b"meta".to_vec()),
             corpus: UnorderedMap::new(b"corpus".to_vec()),
-            authors: UnorderedMap::new(b"authors".to_vec()),
         }
     }
 }
 
 #[near_bindgen]
 impl Wiki {
+    pub fn reset(&mut self) {
+        self.meta.clear();
+        self.corpus.clear();
+    }
+
     // Get an article
-    pub fn get_article(&self, article_id: u64) -> (String, String, String) {
-        let author = self.authors.get(&article_id).unwrap_or(String::from("Author not available"));
-        let title = self.titles.get(&article_id).unwrap_or(String::from("Title not available"));
-        let content = self.corpus.get(&article_id).unwrap_or(String::from("Content not available"));
-        return (author, title, content);
+    pub fn get_article(&self, article_id: u64) -> Article {
+        let meta = self.meta.get(&article_id).unwrap_or(ArticleMeta {
+            title: String::from("Not found"),
+            author: String::from("Not found"),
+        });
+        let content = self
+            .corpus
+            .get(&article_id)
+            .unwrap_or(String::from("Content not available"));
+
+        return Article {
+            id: article_id,
+            title: meta.title,
+            content: content,
+            author: meta.author,
+        };
     }
 
     // Get a list of articles
-    /*
-    pub fn get_articles(&self) -> &Vector<(u64, String, String)> {
-        let titles = self.titles.to_vec();
-        let authors = self.authors.values_as_vector();
-        return titles.iter().zip(authors);
+    pub fn get_articles(&self) -> Vec<(u64, ArticleMeta)> {
+        return self.meta.to_vec();
     }
-    */
 
     // Create a new article for 2 NEARs
     #[payable]
     pub fn create_article(&mut self, title: String, content: String) -> u64 {
+        // Should panic when not enough fund
+        
         let author = env::current_account_id();
         let published_date = env::block_timestamp();
 
         env::log(format!("Article created at {}", published_date).as_bytes());
 
-        let article_id = self.len;
+        let article_id = self.corpus.len();
 
-        self.titles.insert(&article_id, &title);
+        let meta = ArticleMeta {
+            title: title,
+            author: author,
+        };
+
+        self.meta.insert(&article_id, &meta);
         self.corpus.insert(&article_id, &content);
-        self.authors.insert(&article_id, &author);
 
         return article_id;
     }
 
     // Edit an existing article for 0.5 NEARs
-    /*
     #[payable]
-    pub fn update_article(&mut self, article_id: &u64, content: String) {
+    pub fn update_article(&mut self, article_id: u64, content: String) {
         // Should panic when not enough fund
 
-        match self.corpus.get(article_id) {
-            Some(mut article) => {
-                article.content = content;
-                self.corpus.insert(article_id, &article);
+        match self.corpus.get(&article_id) {
+            Some(_old_content) => {
+                self.corpus.insert(&article_id, &content);
             }
             None => env::log("Article not found".as_bytes()),
         }
@@ -82,7 +107,6 @@ impl Wiki {
     // Upvote or download an article
 
     // Donate to an article
-    */
 }
 
 #[cfg(not(target_arch = "wasm32"))]
