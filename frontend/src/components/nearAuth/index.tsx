710c842b85ecd23d54271d05e79078467280096b
Kha <nlhkha@gmail.com>
2022-04-17 14:22:11 +0000

    Authenticate app with NEAR wallet

--- frontend/src/components/nearAuth/index.tsx
+++ frontend/src/components/nearAuth/index.tsx
@@ -1,19 +1,47 @@
 import { WalletConnection } from "near-api-js"
-import React, { createContext, useState, useEffect, ReactNode } from "react"
-import { getNearWallet } from "../../near-wallet"
+import React, { createContext, useContext, useState, useEffect, ReactNode } from "react"
+import { getNearWallet, signIn, signOut } from "../../near-wallet"
 
-const NEARContext = createContext<WalletConnection | undefined>(undefined)
+type IWallet = {
+    authenticated: boolean
+    login: () => Promise<void>
+    logout: () => void
+}
 
-export default function (props: { children: ReactNode }) {
+const NEARContext = createContext<IWallet | undefined>(undefined)
+
+export function useWallet() {
+    return useContext(NEARContext)
+}
+
+export function WalletProvider(props: { children: ReactNode }) {
     const [wallet, setWallet] = useState<WalletConnection | undefined>(undefined)
 
+    const [authenticated, setAuthenticated] = useState<boolean>(false)
+
+    async function login() {
+        await signIn(wallet!)
+    }
+
+    function logout() {
+        signOut(wallet!)
+        setAuthenticated(wallet!.isSignedIn())
+    }
+
     useEffect(function () {
-        getNearWallet().then(setWallet)
+        getNearWallet().then(w => {
+            setWallet(w)
+            setAuthenticated(w.isSignedIn())
+        })
     }, [])
 
-    return <>
-        <NEARContext.Provider value={wallet}>
+    const iWallet: IWallet = { authenticated, login, logout }
+
+    if (wallet === undefined) {
+        return <div>Loading wallet...</div>
+    } else {
+        return <NEARContext.Provider value={iWallet}>
             {props.children}
         </NEARContext.Provider>
-    </>
+    }
 }
--- frontend/src/layouts/index.tsx
+++ frontend/src/layouts/index.tsx
@@ -1,20 +1,29 @@
 import React, { ReactNode } from "react"
+import { WalletProvider, useWallet } from "../components/nearAuth"
 
 import { header, links, link, linkAction, branding } from "./index.module.css"
 
+function HeadNav() {
+    const wallet = useWallet()
+
+    return <nav className={header}>
+        <div className={branding}>Blockipedia</div>
+        <div className={links}>
+            <a className={link}>Our story</a>
+            <a className={link}>Write</a>
+            {wallet!.authenticated ?
+                <a className={link} onClick={wallet!.logout}>Sign-out</a> :
+                <a className={link} onClick={wallet!.login}>Sign-in</a>}
+            <a className={linkAction}>Get started</a>
+        </div>
+    </nav>
+}
+
 export default function (props: { children: ReactNode }) {
-    return <>
-        <nav className={header}>
-            <div className={branding}>Blockipedia</div>
-            <div className={links}>
-                <a className={link}>Our story</a>
-                <a className={link}>Write</a>
-                <a className={link} onClick={() => alert(123)}>Sign-in</a>
-                <a className={linkAction}>Get started</a>
-            </div>
-        </nav>
+    return <WalletProvider>
+        <HeadNav />
         <div>
             {props.children}
         </div>
-    </>
+    </WalletProvider>
 }
