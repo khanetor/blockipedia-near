88d3adcaac812512172b63492f08ddf50f681232
Kha <nlhkha@gmail.com>
2022-04-21 11:57:31 +0000

    getStarted button

--- frontend/src/layouts/index.tsx
+++ frontend/src/layouts/index.tsx
@@ -1,11 +1,19 @@
 import React, { ReactNode } from "react"
-import { Link } from "gatsby"
+import { Link, navigate } from "gatsby"
 import { WalletProvider, useWallet } from "../components/nearAuth"
 
 import { header, links, link, linkAction, branding } from "./index.module.css"
 
 function HeadNav() {
     const wallet = useWallet()
+
+    function getStarted() {
+        if (wallet?.authenticated) {
+            navigate("/create")
+        } else {
+            wallet?.login()
+        }
+    }
 
     return <nav className={header}>
         <Link className={branding} to="/">Blockipedia</Link>
@@ -15,7 +23,7 @@ function HeadNav() {
             {wallet!.authenticated ?
                 <a className={link} onClick={wallet!.logout}>Sign-out</a> :
                 <a className={link} onClick={wallet!.login}>Sign-in</a>}
-            <a className={linkAction}>Get started</a>
+            <a className={linkAction} onClick={getStarted}>Get started</a>
         </div>
     </nav>
 }
