4106c7942451a20b8c3440226dc89241e5418905
Kha <nlhkha@gmail.com>
2022-04-23 15:55:18 +0300

    Read interface (#31)
    * read interface, vote, donate
    
    * set title
    
    * make modal handle visibility
    
    * add animation when open donation dialog

---
+++ frontend/src/pages/read/[id].module.css.d.ts
@@ -0,0 +1,11 @@
+declare const styles: {
+  readonly "readingView": string;
+  readonly "headerSection": string;
+  readonly "headerInfo": string;
+  readonly "headerRating": string;
+  readonly "headerAction": string;
+  readonly "titleSection": string;
+  readonly "contentSection": string;
+};
+export = styles;
+
