3e90f7e8bad7afaee38ee61580f62a9460095361
Kha <nlhkha@gmail.com>
2022-04-23 15:24:30 +0300

    Working version of editor (#29)

--- frontend/src/pages/create/index.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from "react"
-import { NEARAuth } from "../../components/nearAuth"
-
-export default function() {
-    return <NEARAuth>
-        You are authenticated to NEAR
-    </NEARAuth>
-}
---
+++ frontend/src/pages/write/index.module.css.d.ts
@@ -0,0 +1,9 @@
+declare const styles: {
+  readonly "composer": string;
+  readonly "heading": string;
+  readonly "bodyComposer": string;
+  readonly "editor": string;
+  readonly "preview": string;
+};
+export = styles;
+
