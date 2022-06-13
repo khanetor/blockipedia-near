48f6923373f9ebbaddfe40326b562b4fecfa3e58
Kha <nlhkha@gmail.com>
2022-06-14 00:22:32 +0200

    add styling for anchor in markdown (#58)

--- frontend/gatsby-config.ts
+++ frontend/gatsby-config.ts
@@ -13,7 +13,7 @@ import type { GatsbyConfig } from "gatsby";
 const config: GatsbyConfig = {
   siteMetadata: {
     title: process.env.GATSBY_APP_NAME,
-    siteUrl: `${process.env.GATSBY_HOSTNAME!}${process.env.GATSBY_PATH_PREFIX!}`
+    siteUrl: `${process.env.GATSBY_HOSTNAME!}`
   },
   pathPrefix: process.env.GATSBY_PATH_PREFIX, // e.g. `/blockipedia-near` when served by GitHub Pages
   plugins: [
